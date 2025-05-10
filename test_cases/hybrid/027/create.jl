351d9d6045923761d0a02c993f3a494e22945c80
sebapersson <sebastian.persson1@gmail.com>
2025-05-10 12:14:43 +0100

    Format

--- test_cases/hybrid/027/create.jl
+++ test_cases/hybrid/027/create.jl
@@ -6,7 +6,8 @@ llh_id = :pre_ODE1
 condition_table_id = :Table1
 observable_table_id = :Table1
 sbml_id = :lv_reference
-petab_parameters_ids = [:alpha, :delta, :beta, :net1_input_pre1, :net1_input_pre2, :net1_layer1_weight_freeze]
+petab_parameters_ids = [
+    :alpha, :delta, :beta, :net1_input_pre1, :net1_input_pre2, :net1_layer1_weight_freeze]
 freeze_info = Dict(:layer1 => [:weight])
 # Mapping and hybridization generally differ between tests and must thus be hand-coded
 mapping_table = DataFrame(
