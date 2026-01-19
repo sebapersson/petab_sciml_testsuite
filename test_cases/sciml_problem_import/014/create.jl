7e80c0010051e6d241f31eacbab3f876b50db624
sebapersson <sebastian.persson1@gmail.com>
2026-01-19 11:17:17 +0000

    Update test-case 014 and 015

--- test_cases/sciml_problem_import/014/create.jl
+++ test_cases/sciml_problem_import/014/create.jl
@@ -14,8 +14,7 @@ petab_parameters_ids = [:alpha, :delta, :beta]
 mapping_table = DataFrame(
     petabEntityId = ["input0", "net3_output1", "net3_ps"],
     modelEntityId = ["net3.inputs[0]", "net3.outputs[0][0]", "net3.parameters"])
-hybridization_table = DataFrame(targetId = ["gamma"],
-    targetValue = ["net3_output1"])
+hybridization_table = DataFrame(targetId = ["gamma"], targetValue = ["net3_output1"])
 
 save_hybrid_test_values(
     @__DIR__, nets_info, ode_id, llh_id, petab_parameters_ids; input_file_id = input_id
